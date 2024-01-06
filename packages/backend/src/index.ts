c9a4b2d6fc9bb9d17dab69d16b350f320a9b9089
Omri Barouch <omby8888@gmail.com>
2024-01-06 18:39:08 +0200

    create collections if not exists on load

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -6,6 +6,10 @@ import compression = require('compression');
 import cors = require('cors');
 import apiRouter from './router';
 import mongoose from 'mongoose';
+import { UserModel } from "./models/user";
+import { EventSchemaModel } from "./models/eventSchema";
+import { DashboardModel } from "./models/dashboard";
+import { EventModel } from "./models/event";
 require('dotenv').config();
 
 const app = express();
@@ -14,16 +18,24 @@ app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
+app.use('/api', apiRouter);
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-  console.log('Server running on http://localhost:8080/');
-});
-
 mongoose.Promise = Promise;
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_URL).then(() => console.log("Connected to mongo db successfully!"));
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  console.log("Connected to mongo db successfully!");
+  Promise.all([
+    UserModel.createCollection,
+    EventSchemaModel.createCollection,
+    EventModel.createCollection,
+    DashboardModel.createCollection
+  ])
+      .then(() => {
+        server.listen(8080, () => {
+          console.log('Server running on http://localhost:8080/');
+        });
+      });
+});
 mongoose.connection.on('error', (error: Error) => console.log(error));
-
-app.use('/api', apiRouter);
